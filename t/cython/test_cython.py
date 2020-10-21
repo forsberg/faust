8bf391528f85cf71407f4d590d277906ef8e72bf
Erik Forsberg <erik.forsberg@ferroamp.se>
2020-10-21 08:27:17 +0200

    Show tox logs after success. Adds extra test that verifies Cython is enabled

---
+++ t/cython/test_cython.py
@@ -0,0 +1,23 @@
+import os
+class test_Cython:
+    """Ensure Cython is enabled, and Cython modules were compiled
+    Only to be run in cython environment to ensure cython tests are actually
+    testing Faust with Cython enabled"""
+    def test_cython_enabled(self):
+        try:
+            from Cython.Build import cythonize
+        except ImportError:
+            USE_CYTHON = False
+        else:
+            USE_CYTHON = os.environ.get('USE_CYTHON', True)
+
+
+        if os.environ.get('NO_CYTHON'):
+            USE_CYTHON = False
+
+        assert USE_CYTHON == True
+
+    def test_import(self):
+        from faust._cython.windows import HoppingWindow
+        hopping = HoppingWindow(60, 60)
+        # No assert, just check that we could import and init
